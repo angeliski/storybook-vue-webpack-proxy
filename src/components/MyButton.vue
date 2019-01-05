2b7f7a92d6c39563e21f9cbf9067faff92a391fc
Rogerio Angeliski <angeliski@hotmail.com>
2019-01-04 23:32:51 -0200

    Storybook using proxy webpack vue

---
+++ src/components/MyButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button class="button is-primary" @click="onClick">
+      <slot></slot>
+    </button>
+    <img :src="cat" v-if="cat" width="300" height="300">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-button",
+
+  data() {
+    return {
+      cat: ""
+    };
+  },
+  methods: {
+    onClick() {
+      this.$http
+        .get("/api")
+        .then(({ data }) => (this.cat = data.file));
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  border: 1px solid #eee;
+  border-radius: 3px;
+  background-color: #ffffff;
+  cursor: pointer;
+  font-size: 15pt;
+  padding: 3px 10px;
+  margin: 10px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,32 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    port: 8080,
+    logLevel: 'debug',
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    proxy: {
+      '/api': {
+        logLevel: 'debug',
+        target: 'https://aws.random.cat/meow',
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/*': {
+        logLevel: 'debug',
+        target: 'http://localhost:6006/',
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/story': '',
+          '^/': ''
+        }
+      },
+    },
+  },
 }
